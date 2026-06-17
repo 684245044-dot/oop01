a1653fdf103fe0c2b8947ddaf2db7b60977207af
684245044-dot <684245044@webmail.npru.ac.th>
2026-06-17 11:58:21 +0700

    Add files via upload

---
+++ leb1_4.ts
@@ -0,0 +1,8 @@
+let income: number = 50000;
+let tax: number = 0.07;
+let taxAmount: number = income * tax;
+let netIncome: number = income - taxAmount;
+console.log("Income:", income);
+console.log("Tax Rate:", tax);
+console.log("Tax Amount:", taxAmount);
+console.log("Net Income:", netIncome);
